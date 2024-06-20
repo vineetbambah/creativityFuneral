c9038afb803499ff46249d8cf9fe46ea4b0dd5b5
neozvbambah <neozvbambah@gmail.com>
2024-06-20 23:58:51 +0530

    fix-for-build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet" />
     </Head>
     <main className="grid grid-row mt-40 place-content-center">
-      <h1 className="font-medium text-3xl text-red-500 justify-self-end hover:rotate-6">creativity's funeral</h1>
+      <h1 className="font-medium text-3xl text-red-500 justify-self-end hover:rotate-6">creativity&apos;s funeral</h1>
       <h2 className="font-semibold font-inter dark:text-white text-3xl w-11/12 mt-5 m-auto">what kind of project do u have on ur mind today?</h2>
       <input type="text" name="query" className="border border-red-500 rounded-md dark:border-red-800 focus:border-none p-2 mt-10 dark:bg-zinc-950" />
       <button className="bg-red-500 hover:bg-red-600 m-auto p-4 text-white text-xl rounded-md w-64 mt-12">generate <span className="font-bold">ideas</span></button>
