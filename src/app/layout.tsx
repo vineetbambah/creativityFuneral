5c58f1c7542d8e9c5141aa63932475b5c4a1a2a7
neozvbambah <neozvbambah@gmail.com>
2024-06-23 19:25:34 +0530

    frontend-updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
 const inter = Inter({ subsets: ["latin"] });
-
+import { ThemeProvider } from "./theme-provider";
 export const metadata: Metadata = {
   title: "Creativity's Funeral",
   description: "AI web app using Gemini to list ideas",
@@ -15,8 +14,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="bg-dot-black/[0.1] dark:bg-dot-white/[0.1] dark:bg-black">{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className="bg-dot-black/[0.1] dark:bg-dot-white/[0.1] dark:bg-black">
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+      </body>
+      
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { LinkPreview } from "./link-preview";
+import { ModeToggle } from "./theme-toggle";
 export default function Home() {
 
   return (
@@ -10,13 +11,14 @@ export default function Home() {
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet" />
     </Head>
     <main className="grid grid-row mt-40 place-content-center">
-      <h1 className="font-medium text-3xl text-red-500 justify-self-end hover:rotate-6">creativity&apos;s funeral</h1>
+      <span className="fixed top-0 right-0 m-2"><ModeToggle/></span>
+      <h1 className="font-medium text-3xl text-red-500 justify-self-end transition duration-50 ease-linear hover:rotate-6">creativity&apos;s funeral</h1>
       <h2 className="font-semibold font-inter dark:text-white text-3xl w-11/12 mt-5 m-auto">what kind of project do u have on ur mind today?</h2>
-      <input type="text" name="query" className="border border-red-500 rounded-md dark:border-red-800 focus:border-none p-2 mt-10 dark:bg-zinc-950" />
-      <button className="bg-red-500 hover:bg-red-600 m-auto p-4 text-white text-xl rounded-md w-64 mt-12">generate <span className="font-bold">ideas</span></button>
+      <input type="text" name="query" className="border border-red-500 rounded-md transition duration-500 ease-in shadow-blur focus:shadow-red-500 dark:focus:shadow-red-400 dark:border-red-800 focus:border-red-500 focus:outline-none dark:focus:outline-none p-2 mt-10 dark:bg-zinc-950" />
+      <button className="bg-red-500 hover:bg-red-600 m-auto p-4 text-white text-xl transition duration-500 ease-in shadow-blur2 hover:shadow-blue-400 dark:hover:shadow-blue-800 rounded-md w-64 mt-12">generate <span className="font-bold">ideas</span></button>
     </main>
     <footer>
-      <h1 className="absolute bottom-0 right-0 mr-2 text-lg dark:text-white">built by <LinkPreview url='https://github.com/vineetbambah' className="hover:underline ">@vineetbambah</LinkPreview></h1>
+      <span className="fixed bottom-0 right-0 mr-2 flex"><h1 className="text-lg dark:text-white">built by @</h1> <LinkPreview url='https://github.com/vineetbambah' className="dark:text-white text-lg hover:underline ">vineetbambah</LinkPreview></span>
     </footer>
     </>
   );
